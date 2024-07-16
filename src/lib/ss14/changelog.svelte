55dda52965314463b2ec40f36d3f44347f06250a
Kei Shin <misterkeishin@gmail.com>
2024-07-16 19:37:26 -0400

    Adds isNewDay and loading indicator

--- src/lib/ss14/changelog.svelte
+++ src/lib/ss14/changelog.svelte
@@ -3,6 +3,7 @@
 import { onMount } from "svelte";
 import yaml from "js-yaml";
 import config from "$lib/config.js"
+import { Skeleton } from "$lib/components/ui/skeleton/index.js";
 
     const clClassMap = {
     Add: 'w-6 fa-solid fa-plus text-green-600',
@@ -10,7 +11,11 @@ import config from "$lib/config.js"
     Tweak: 'w-6 fa-solid fa-wrench text-slate-600',
     Fix: 'w-6 fa-duotone fa-bug-slash'
     };
-
+    function isNewDay(currentTime, previousTime) {
+        const currentDate = new Date(currentTime).toISOString().split('T')[0];
+        const previousDate = previousTime ? new Date(previousTime).toISOString().split('T')[0] : null;
+        return previousDate === null || currentDate !== previousDate;
+    }
     let changelog = [];
     $:changelog
     async function getChangelog() {
@@ -29,10 +34,50 @@ onMount(async () => {
 });
 </script>
 
-<div class="max-h-96 border-accent border-[1px] p-1 overflow-auto">
+<div class="sm:h-96 max-h-96 border-accent border-[1px] p-1 {changelog.length === 0 ? "overflow-hidden" : "overflow-auto"}">
+    {#if changelog.length === 0}
+    <div class="flex items-center space-x-4">
+        <div class="space-y-2 p-1">
+          <Skeleton class="h-8 w-52 pb-4" />
+          <Skeleton class="h-6 w-64 pb-4" />
+          <div class="flex items-center space-x-4">
+            <Skeleton class="h-4 w-4 pb-4" />
+            <Skeleton class="h-6 w-[768px] pb-4" />
+          </div>
+          <div class="flex items-center space-x-4">
+            <Skeleton class="h-4 w-4 pb-4" />
+            <Skeleton class="h-12 w-[768px] pb-4" />
+          </div>
+          <Skeleton class="h-6 w-52 pb-4" />
+          <div class="flex items-center space-x-4">
+            <Skeleton class="h-4 w-4 pb-4" />
+            <Skeleton class="h-6 w-[512px] pb-4" />
+          </div>
+          <div class="flex items-center space-x-4">
+            <Skeleton class="h-4 w-4 pb-4" />
+            <Skeleton class="h-6 w-[256px] pb-4" />
+          </div>
+          <div class="flex items-center space-x-4">
+            <Skeleton class="h-4 w-4 pb-4" />
+            <Skeleton class="h-6 w-[512px] pb-4" />
+          </div>
+          <div class="flex items-center space-x-4">
+            <Skeleton class="h-4 w-4 pb-4" />
+            <Skeleton class="h-6 w-[512px] pb-4" />
+          </div>
+          <Skeleton class="h-8 w-52 pb-4" />
+          <Skeleton class="h-6 w-64 pb-4" />
+        </div>
+    </div>
+    {:else}
     {#each changelog as item, index}
         <div class="pb-4 {index % 2 === 0 ? 'bg-background' : 'bg-background/90'}">
-            <h4>{item.author}</h4>
+            {#if isNewDay(item.time, changelog[index - 1]?.time) === true}
+            <h4 class="text-accent-foreground font-bold">{new Date(item.time).toLocaleDateString('en-US', { timeZone: 'UTC', month: 'long', day: 'numeric', year: 'numeric' })}</h4>
+            <h4 class="font-semibold">{item.author}</h4>
+            {:else}
+            <h4 class="font-semibold">{item.author}</h4>
+            {/if}
             {#each item.changes as change}
             <div class="flex items-center gap-1">
                 <i class={clClassMap[change.type] || 'fa-solid fa-question'}/>
@@ -41,4 +86,5 @@ onMount(async () => {
             {/each}
         </div>
     {/each}
+    {/if}
 </div>
