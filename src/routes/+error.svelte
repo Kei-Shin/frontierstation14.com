f24ab83a8cdb94eddd185dfdaee6b4e54deadf7a
Kei Shin <misterkeishin@gmail.com>
2024-08-11 13:04:45 -0400

    404s Exist

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { Button } from "$lib/components/ui/button";
+</script>
+
+<div class="flex h-full justify-center items-center">
+    <div class="w-fit m-4 rounded-lg border-[1px] border-[#343438] bg-background/90 p-4">
+        <div class="flex flex-col items-center">
+            <img class="w-16 h-16" src="/spacecat_dead.png" alt="Spacesuit cat with a broken helmet">
+            <h1 class="mt-8 mb-4">404 Error!</h1>
+            <p class="mb-6">You shouldn't make a habit of getting lost in space.</p>
+            <Button variant="spacebutton" href="/">
+                <span>Return Home</span>
+            </Button>
+        </div>
+    </div>
+</div>
